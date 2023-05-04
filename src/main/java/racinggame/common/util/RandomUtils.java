c92f0117fa2179c9a9d9a6a7b0d08c7d4c664f9a
진우 <nice7677@naver.com>
2023-05-04 13:10:39 +0900

    refactor: 4단계 마지막 코멘트 수정

--- src/main/java/racinggame/common/util/RandomUtils.java
+++ src/main/java/racinggame/common/util/RandomUtils.java
@@ -6,7 +6,6 @@ public class RandomUtils {
 
     private static final int BOUND = 10;
     private static final Random random = new Random();
-
     public static int generate() {
         return random.nextInt(BOUND);
     }
